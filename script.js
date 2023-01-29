9828153ef79310ecea46ce5922758014ded53cae
OrangeRootkit <katkovdv88@gmail.com>
2023-01-29 18:26:22 +0300

    update

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 'use strict'
 
 const inputLine =  document.querySelector('.header__input');
-// const cardText = document.querySelectorAll('.card__text');
-// cardText.style.color = 'red';
+const cardText = document.querySelectorAll('.card__text');
+console.log(cardText);
 
 const changeColor = (event) => {
-    inputLine.style.color = 'red'
-}
+     inputLine.style.color = 'red'
+ }
 
-const searchCard = (event) => {
-    inputLine.style.color = 'red'
-}
+// const searchCard = (event) => {
+//     inputLine.style.color = 'red'
+// }
 
 
 inputLine.addEventListener("click", changeColor());
 
 // let handler = changeColor();
-
-
-
+console.log('hi');
+console.log('sdt')
+console.log('he')
 // ()=> {
 //     inputLine.style.color = 'red';
 // });
